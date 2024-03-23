32a4bdecee7c42e4c0a49a422460cc229f0e461c
Rohit Ramesh <rrohitramesh710@gmail.com>
2024-03-23 12:13:47 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# This a Frame on Base to Mint NFTs
+
+This is a Farcaster Frame that allows your Farcaster users to mint an NFT on the Base directly to their email address. A custodial wallet is generated for the user the moment they enter their email address. The mint is fully gasless and provides a native Web 2 experience.
+
+All you need to do is like the cast and re-cast it, and then enter your Email address and click on Mint to mint the NFT. The NFT will then be delivered to your wallet address in a few seconds. Users can also choose to mint a special NFT using their credit card. This allows creators to segregate their users/audience in different tiers.
---
+++ src/app/page.tsx
@@ -0,0 +1,49 @@
+import { getFrameMetadata } from "@coinbase/onchainkit";
+import type { Metadata } from "next";
+
+const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL;
+
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      action: "post",
+      label: "Mint to an email address",
+      target: `${NEXT_PUBLIC_URL}/api/frame/`,
+    },
+    {
+      action: "link",
+      label: "Mint with Credit Card",
+      target:
+        "https://staging.crossmint.com/collections/dynamic-nft-frame/drop",
+    },
+  ],
+  image: {
+    src: `${NEXT_PUBLIC_URL}/default.png`,
+    aspectRatio: "1.91:1",
+  },
+  input: {
+    text: "Enter your Email Address",
+  },
+  postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+});
+
+export const metadata: Metadata = {
+  title: "Mint an NFT on Base",
+  description: "Mint an NFT on Base",
+  openGraph: {
+    title: "Mint an NFT on Base",
+    description: "Powered by Crossmint and Base",
+    images: [`${NEXT_PUBLIC_URL}/default.png`],
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
+
+export default function Page() {
+  return (
+    <>
+      <h1>Mint an NFT on Base</h1>
+    </>
+  );
+}
