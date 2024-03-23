343476f719119f863af0d625f060bdc993ae9962
Rohit Ramesh <rrohitramesh710@gmail.com>
2024-03-23 14:12:42 +0530

    framesjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,19 @@
-import { getFrameMetadata } from "@coinbase/onchainkit";
-import type { Metadata } from "next";
+// app/page.tsx
+import { fetchMetadata } from "frames.js/next";
 
-const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL;
-
-const frameMetadata = getFrameMetadata({
-  buttons: [
-    {
-      action: "post",
-      label: "Mint to an email address",
-    },
-    {
-      action: "link",
-      label: "Mint with Credit Card",
-      target:
-        "https://staging.crossmint.com/collections/dynamic-nft-frame/drop",
-    },
-  ],
-  image: {
-    src: `${NEXT_PUBLIC_URL}/default.png`,
-    aspectRatio: "1.91:1",
-  },
-  input: {
-    text: "Enter your Email Address",
-  },
-  postUrl: `${NEXT_PUBLIC_URL}/api/frame/mint`,
-});
-
-export const metadata: Metadata = {
-  title: "Mint an NFT on Base",
-  description: "Mint an NFT on Base",
-  openGraph: {
-    title: "Mint an NFT on Base",
-    description: "Powered by Crossmint and Base",
-    images: [`${NEXT_PUBLIC_URL}/default.png`],
-  },
-  other: {
-    ...frameMetadata,
-  },
-};
+export async function generateMetadata() {
+  return {
+    title: "My page",
+    // provide full URL to your /frames endpoint
+    ...(await fetchMetadata(
+      new URL(
+        "/frames",
+        process.env.VERCEL_URL || "https://base-frame-xi.vercel.app"
+      )
+    )),
+  };
+}
 
 export default function Page() {
-  return (
-    <>
-      <h1>Mint an NFT on Base</h1>
-    </>
-  );
+  return <span>My existing page</span>;
 }
