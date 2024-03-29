deb27f4c0ac09cd4503d57182a8b5c90781af4a3
Rohit Ramesh <rrohitramesh710@gmail.com>
2024-03-29 12:26:36 +0530

    changes

--- src/app/api/frame/route.ts
+++ src/app/api/frame/route.ts
@@ -31,6 +31,7 @@ export async function POST(req: NextRequest): Promise<Response> {
           buttons: [
             {
               label: "Refresh and try again!",
+              action: "post",
             },
           ],
         })
